82d4381be2cc6a4b0413753d2d99a6a17e5cdee3
Christian Lulaj <lulajchr@cse.msu.edu>
2021-11-17 09:44:11 -0500

    Christian Lulaj's Sound Board Experiment

---
+++ soundboard.js
@@ -0,0 +1,8 @@
+const bowlingAudio = new Audio();
+bowlingAudio.src = "./bowling.wav"
+
+const boxingAudio = new Audio();
+boxingAudio.src = "./boxing_bell_multiple.wav"
+
+const bubblingAudio = new Audio();
+bubblingAudio.src = "./bubbling1.wav"
